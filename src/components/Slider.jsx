1e904a6cdb4dbbcee51362db396b1bbca02ea56b
Tomás <tomasgoizuetadp@gmail.com>
2022-09-25 11:38:29 +0200

    Slide system with animation added

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,12 +1,15 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from "@mui/icons-material";
 import React from "react";
+import { useState } from "react";
 import styled from "styled-components";
+import { sliderItems } from "../data";
 
 const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
   position: relative;
+  overflow: hidden;
 `;
 
 const Arrow = styled.div`
@@ -25,10 +28,14 @@ const Arrow = styled.div`
   margin: auto;
   cursor: pointer;
   opacity: 0.5;
+  z-index: 2;
 `;
 
 const Wrapper = styled.div`
   height: 100%;
+  display: flex;
+  transition: all 1.5s ease;
+  transform: translateX(${props=>props.slideIndex * -100}vw);
 `;
 
 const Slide = styled.div`
@@ -36,6 +43,7 @@ const Slide = styled.div`
   height: 100vh;
   display: flex;
   align-items: center;
+  background-color: ${props => props.bg};
 `;
 const ImgContainer = styled.div`
   height: 100%;
@@ -52,42 +60,51 @@ const InfoContainer = styled.div`
 `;
 
 const Title = styled.h1`
-    font-size: 70px;    
+  font-size: 70px;
 `;
 const Desc = styled.p`
-margin: 50px 0px;
-font-size: 20px;
-font-weight: 500;
-letter-spacing: 2px;
+  margin: 50px 0px;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 2px;
 `;
 const Button = styled.button`
-    padding: 10px;
-    background-color: transparent;
-    font-size: 20px;
-    cursor: pointer;
+  padding: 10px;
+  background-color: transparent;
+  font-size: 20px;
+  cursor: pointer;
 `;
 
 const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+
+  const handleClick = (direction) => {
+    if (direction === "left") {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : sliderItems.length-1);
+    } else {
+      setSlideIndex(slideIndex < (sliderItems.length-1) ? (slideIndex + 1) : 0);
+    }
+  };
   return (
     <Container>
-      <Arrow direction="left">
+      <Arrow direction="left" onClick={() => handleClick("left")}>
         <ArrowLeftOutlined />
       </Arrow>
-      <Wrapper>
-        <Slide>
-          <ImgContainer>
-            <Image src="https://cdn.pixabay.com/photo/2017/09/06/23/04/model-2723443_1280.jpg" />
-          </ImgContainer>
-          <InfoContainer>
-            <Title>FALL SALE</Title>
-            <Desc>
-              DON'T MISS OUR FLASH OFFERS AND GET DISCOUNTS OF UP TO 75%.
-            </Desc>
-            <Button>SHOP NOW</Button>
-          </InfoContainer>
-        </Slide>
+      <Wrapper slideIndex={slideIndex}>
+        {sliderItems.map((item) => (
+          <Slide bg={item.bg}>
+            <ImgContainer>
+              <Image src={item.img} />
+            </ImgContainer>
+            <InfoContainer>
+              <Title>{item.title}</Title>
+              <Desc>{item.desc}</Desc>
+              <Button>SHOW NOW</Button>
+            </InfoContainer>
+          </Slide>
+        ))}
       </Wrapper>
-      <Arrow direction="right">
+      <Arrow direction="right" onClick={() => handleClick("right")}>
         <ArrowRightOutlined />
       </Arrow>
     </Container>
